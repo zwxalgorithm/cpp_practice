b4bd4c44e803fb411bb53cb6804d5bbbc24369d7
jiaoqiyuan <jiaoqiyuan@outlook.com>
2016-05-12 20:10:06 +0800

    First commit

---
+++ 9/practice9-18.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<deque>
+
+using namespace std;
+
+int main()
+{
+	deque<string> sd;
+	string word;
+	while(cin >> word)
+	{
+		sd.push_back(word);
+	}
+	for(auto si = sd.begin(); si != sd.end(); ++si)
+	{
+		cout << *si << endl;
+	}
+	return 0;
+}
