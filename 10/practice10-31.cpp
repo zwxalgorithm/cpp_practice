3fa95bd86886a56ded97976dd472c0b804efeb30
jiaoqiyuan <jiaoqiyuan@outlook.com>
2016-05-14 10:15:04 +0800

    add practice10-31.cpp

---
+++ 10/practice10-31.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<iterator>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	istream_iterator<int> in_iter(cin);
+	istream_iterator<int> eof;
+	vector<int> vi;
+	while(in_iter != eof)
+		vi.push_back(*in_iter++);
+	sort(vi.begin(), vi.end());
+
+	ostream_iterator<int> out_iter(cout, " ");
+	copy(vi.begin(), vi.end(), out_iter);
+
+	return 0;
+}
