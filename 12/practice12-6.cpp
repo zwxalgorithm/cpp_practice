3b19cff6fdb1afd6dee783fbb2c551f6509eb174
jiaoqiyuan <jiaoqiyuan@outlook.com>
2016-05-16 16:04:37 +0800

    add practice12-6 files

---
+++ 12/practice12-6.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+vector<int> *
+new_vector(void)
+{
+	return new (nothrow) vector<int>;
+}
+
+void
+read_ints(vector<int> *pv)
+{
+	int v;
+	while(cin >> v)
+		pv->push_back(v);
+}
+
+void
+print_ints(vector<int> *pv)
+{
+	for(const auto &v : *pv)
+		cout << v << " ";
+	cout << endl;
+}
+
+int 
+main()
+{
+	vector<int> *pv = new_vector();
+	if(!pv)
+	{
+		cout << "内存不足！" << endl;
+		return -1;
+	}
+	
+	read_ints(pv);
+	print_ints(pv);
+	
+	delete pv;
+	pv = nullptr;
+
+	return 0;
+	
+}
